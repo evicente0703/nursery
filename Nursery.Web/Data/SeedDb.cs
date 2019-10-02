14b5fda9357ed3c0993810bf7632b89cae05d677
Edwin Vicente <evicente0703@gmail.com>
2019-10-02 12:19:13 -0600

    Full IRepository

---
+++ Nursery.Web/Data/SeedDb.cs
@@ -0,0 +1,43 @@
+﻿namespace Nursery.Web.Data
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Nursery.Web.Data.Entities;
+
+    public class SeedDb
+    {
+        private readonly DataContext context;
+        private Random random;
+
+        public SeedDb(DataContext context)
+        {
+            this.context = context;
+            this.random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await this.context.Database.EnsureCreatedAsync();
+
+            if (!this.context.Products.Any())
+            {
+                this.AddProduct("iPhone x");
+                this.AddProduct("LGe 9");
+                this.AddProduct("Samsung Meta");
+                await this.context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            this.context.Products.Add(new Product
+            {
+                Name = name,
+                Price = this.random.Next(100),
+                IsAvailabe = true,
+                Stock = this.random.Next(100)
+            });
+        }
+    }
+}
